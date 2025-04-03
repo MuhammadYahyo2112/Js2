97706484e91d58b0fa57c875435909c7399e136b
MuhammadYahyo2112 <yahyo0121@gmail.com>
2025-04-03 14:57:29 +0300

    Add files via upload

---
+++ scrpit.js
@@ -0,0 +1,16 @@
+let savol = prompt('String typde dage malumot kriting')
+
+let savol2 = +prompt('Number typde dege malumot kriting')
+
+let savol3 = prompt('Boolean typedagi malumot kiriting')
+
+
+let javob = alert
+(`
+javob  ${savol}  
+javob ${savol2} 
+javob  ${savol3} 
+
+`)
+
+
